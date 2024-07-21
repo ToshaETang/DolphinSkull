c494a87aa9809439bb3d8d85b440ae6f3d458ad9
ToshaETang <a0320506@gmail.com>
2024-07-22 00:49:31 +0800

    0718

---
+++ cloud/try.sh
@@ -0,0 +1,30 @@
+
+#!/bin/bash
+#PBS -N try0718
+#PBS -o master:$PBS_O_WORKDIR/$PBS_JOBID.out
+#PBS -e master:$PBS_O_WORKDIR/$PBS_JOBID.err
+#PBS -q q24cores
+#PBS -l nodes=1:ppn=1
+cd $PBS_O_WORKDIR
+ulimit -a
+echo '======================================================='
+echo Working directory is $PBS_O_WORKDIR
+echo "Starting on `hostname` at `date`"
+if [ -n "$PBS_NODEFILE" ]; then
+ if [ -f $PBS_NODEFILE ]; then
+ echo "Nodes used for this job:"
+ cat ${PBS_NODEFILE}
+ NPROCS=`wc -l < $PBS_NODEFILE`
+ fi
+fi
+
+module load python-3.12.3
+source venv/bin/activate
+
+export file='../myproject/try.py'
+
+mpirun -hostfile $PBS_NODEFILE -n $NPROCS python3 $file > log
+
+
+echo "Job Ended at `date`"
+echo '======================================================='
